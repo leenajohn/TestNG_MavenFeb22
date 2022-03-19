af4b62bebcbfc89769027fca55ad7dcebd045deb
leenajohn <leena.aju@gmail.com>
2022-03-19 05:37:52 +0000

    Selenium_Maven

---
+++ src/test/java/testcases/HomeTest.java
@@ -0,0 +1,13 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class HomeTest extends BaseClass {
+	
+	@Test
+	public void test1() {
+		
+		System.out.println("Inside Test1");
+	}
+
+}
